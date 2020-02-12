d72d568b2f7c4e019dc2fc5d1e9d8c7fc0b19d51
dalvin <dalvin.j@gmail.com>
2020-02-11 22:29:04 -0800

    working state of code, split up into methods

--- http/text.go
+++ http/text.go
@@ -3,6 +3,13 @@ package main
 import (
   "fmt"
   helpers "github.com/Josiassejod1/http/helpers"
+  "github.com/joho/godotenv"
+  "os"
+  "log"
+  "net/url"
+  "strings"
+  "net/http"
+  "encoding/json"
 )
 
 type Lyric struct {
@@ -21,8 +28,54 @@ type Lyric struct {
    }]`
  */
 
+func init(){
+  if err := godotenv.Load(); err != nil {
+    log.Print("No .env file foud")
+  }
+}
+
 func main() {
-  getLyrics("sicko")
+
+  key, exists := os.LookupEnv("ACCOUNT_SSID")
+
+  if exists {
+    fmt.Println("key exists")
+    fmt.Println(key)
+  }
+
+  pass, exists := os.LookupEnv("ACCOUNT_TOKEN")
+
+  if exists {
+    fmt.Println("pass exists")
+    fmt.Println(pass)
+  }
+  md := getLyrics("sicko")
+  lyric := md.([]interface{})[0].(map[string]interface{})["lyric"]
+  urlStr := "https://api.twilio.com/2010-04-01/Accounts/"+ key +"/Messages.json"
+
+  fmt.Println(lyric.(string))
+  msgData := url.Values{}
+  msgData.Set("To","") //Number to send
+  msgData.Set("From","") //Number from
+  msgData.Set("Body",  "TEST: " + lyric.(string))
+  msgDataReader := *strings.NewReader(msgData.Encode())
+  client := &http.Client{}
+  req, _ := http.NewRequest("POST", urlStr, &msgDataReader)
+  req.SetBasicAuth(key, pass)
+  req.Header.Add("Accept", "application/json")
+  req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+  resp, _ := client.Do(req)
+
+  if (resp.StatusCode >= 200 && resp.StatusCode < 300) {
+  var data map[string]interface{}
+  decoder := json.NewDecoder(resp.Body)
+  err := decoder.Decode(&data)
+  if (err == nil) {
+    fmt.Println(data["sid"])
+  }
+} else {
+  fmt.Println(resp.Status);
+}
 }
 
 /*
@@ -34,7 +87,7 @@ func main() {
 */
 
 
-func getLyrics(url string) {
+func getLyrics(url string) interface{}{
   urlStr := fmt.Sprintf("https://cactus-chorus.herokuapp.com/api/v1/lyrics/%s", url)
 
   var lyric []Lyric
@@ -42,4 +95,5 @@ func getLyrics(url string) {
   lyric_array := helpers.DeconstructJson(lyric, resp)
 
   fmt.Printf("\n Lyrics : %+v", lyric_array)
+  return lyric_array
 }
